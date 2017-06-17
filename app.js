c6e02775fe117cad51c51b76aed5bdcd54a0cf72
danielbastos11 <danielbastos@live.com>
2017-06-17 17:07:59 -0300

    Add animação corações

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
 (function () {
 'use strict';
 
+$(document).ready(function () {
 
+  console.log(document);
+  let $heart = $('.animation.stage .heart');
+
+  $heart.each((i, elm) => assignTo(elm));
+  $heart.on('animationend', function () {
+    assignTo($(this));
+  });
+});
+
+let assignTo = elm => {
+  let anim = 'heart' + Math.floor(Math.random() * 5);
+  let dur = 2 + Math.ceil(Math.random() * 5);
+
+  let $heart = $(elm);
+  let lastAnim = $heart.css('animation-name');
+
+  while (anim == lastAnim) {
+    anim = 'heart' + Math.floor(Math.random() * 5);
+  }
+
+  $heart.css('animation-name', anim);
+  $heart.css('animation-duration', dur + 's');
+};
 
 }());
